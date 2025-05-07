3141a2644c90f39ae1c4832c20ea491b9a3fdb3c
theAugust1st <bhote666@gmail.com>
2025-05-07 13:24:36 +0545

    functionality of PakingList

--- src/components/PakingList.js
+++ src/components/PakingList.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function PakingList() { 
-
+function PakingList() {
+  const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+  ];
   return (
-    <div  className='list'>
-        <ul>
-        </ul>
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return <Item key={item.id} item={item} />;
+        })}
+      </ul>
     </div>
-  )
+  );
 }
-
-export default PakingList
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {''}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+export default PakingList;
